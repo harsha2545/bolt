519288826e3707c25f0e13506eff72efed68048f
root <root@ip-172-31-46-183.ec2.internal>
2019-07-09 06:39:50 +0000

    bolt

---
+++ src/main/java/com/bolt/App.java
@@ -0,0 +1,13 @@
+package com.bolt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
